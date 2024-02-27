d7398a67e6b818b4474ab29f90381931de93f5ea
charlotte <charlottelee849@gmail.com>
2024-02-27 20:06:04 +0800

    Updates

--- assets/HomeView-f6S8TZ4M.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as s,a as t}from"./index-T-IDYSQ5.js";const c={},a={class:"home"},n=t("h1",null,"Hello, This is Home Page.",-1),_=[n];function r(i,d,l,p,h,m){return s(),o("div",a,_)}const u=e(c,[["render",r]]);export{u as default};
